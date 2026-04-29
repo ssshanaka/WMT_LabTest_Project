66f426f1b7ced9484e9a9a70e53b57ff37590a71
Sandaruwan Shanaka <perolopessthesoosa@gmail.com>
2026-04-29 15:14:27 +0530

    Update CORS configuration and API URL for production environment

--- frontend/src/api/itemApi.js
+++ frontend/src/api/itemApi.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API = axios.create({
   baseURL:
     import.meta.env.VITE_API_URL ||
-    "https://wmtlabtestproject-production-dbfb.up.railway.app/api",
+    "https://WMT_LabTest_Project.railway.app/api",
 });
 
 export const getItems = () => API.get("/items");
